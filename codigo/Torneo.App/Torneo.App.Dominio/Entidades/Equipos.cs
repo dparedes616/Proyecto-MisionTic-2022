98c28db51b950ff03bfb1a278fa47d8d9fe56a1e
Daniel Alberto Paredes Peralta <dparedes616@gmail.com>
2022-08-23 20:21:47 -0500

    Se borran archivos y solo se deja codigo

---
+++ codigo/Torneo.App/Torneo.App.Dominio/Entidades/Equipos.cs
@@ -0,0 +1,9 @@
+namespace Torneo.App.Dominio
+{
+    public class Equipos
+    {
+        public int Id { get; set; }
+        public string nombre { get; set; }
+        
+    }
+}
---
+++ codigo/Torneo.App/Torneo.App.Dominio/Entidades/Municpios.cs
@@ -0,0 +1,8 @@
+namespace Torneo.App.Dominio
+{
+    class Municpios
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
