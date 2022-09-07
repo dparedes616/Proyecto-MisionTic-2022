0b82e7c29b56936074fb34b2b6e0dfd468f34ea3
Djnavas95 <daniel.j.navarrete.11@gmail.com>
2022-09-06 19:19:17 -0500

    corregido_This One

---
+++ Torneo.App/Torneo.App.Persistencia/AppRepositorios/IRepositorioMunicipio.cs
@@ -0,0 +1,9 @@
+﻿namespace Torneo.App.Persistencia;
+using Torneo.App.Dominio;
+public class Class1
+{
+    public interface IRepositorioMunicipio
+    {
+        public Municipio AddMunicipio(Municipio municipio);
+    }
+}
